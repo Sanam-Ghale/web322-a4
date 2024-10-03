9687217b785da7b387988d5066dd657bad357302
Netra Rana Magar <nrm7007@gmail.com>
2024-10-03 16:41:44 -0400

    Initial commit

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+
+WEB322 – Assignment 02
+I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
+
+Name: Netra Rana Magar
+Student ID: 117314237
+Date: 03/10/2024
+Cyclic Web App URL: _______________________________________________________
+GitHub Repository URL: https://github.com/nrm7007/web322-app.git
+
+********************************************************************************/ 
+
 const express = require('express');
 const path = require('path');
 const storeService = require('./store-service'); // Require the store-service module
