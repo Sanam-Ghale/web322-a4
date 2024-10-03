e162a14bd8156d5759a9e9253dc3e53f64711063
Netra Rana Magar <nrm7007@gmail.com>
2024-10-03 14:34:00 -0400

    Implemented store-service.js module for data handling

--- store-service.js
+++ store-service.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
+let items = [];
+let categories = [];
+
 // Helper function to read JSON data from a file
 function readJSONFile(filePath) {
     return new Promise((resolve, reject) => {
@@ -14,18 +17,58 @@ function readJSONFile(filePath) {
     });
 }
 
+// Function to initialize the service
+function initialize() {
+    return Promise.all([
+        readJSONFile(path.join(__dirname, 'data', 'categories.json')).then(data => {
+            categories = data;
+        }),
+        readJSONFile(path.join(__dirname, 'data', 'items.json')).then(data => {
+            items = data;
+        })
+    ]).catch(err => {
+        throw new Error("Error reading data files: " + err);
+    });
+}
+
 // Function to get all categories
 function getCategories() {
-    return readJSONFile(path.join(__dirname, 'data', 'categories.json'));
+    return new Promise((resolve, reject) => {
+        if (categories.length === 0) {
+            reject("No categories found");
+        } else {
+            resolve(categories);
+        }
+    });
 }
 
 // Function to get all items
-function getItems() {
-    return readJSONFile(path.join(__dirname, 'data', 'items.json'));
+function getAllItems() {
+    return new Promise((resolve, reject) => {
+        if (items.length === 0) {
+            reject("No items found");
+        } else {
+            resolve(items);
+        }
+    });
+}
+
+// Function to get published items
+function getPublishedItems() {
+    return new Promise((resolve, reject) => {
+        const publishedItems = items.filter(item => item.published === true);
+        if (publishedItems.length === 0) {
+            reject("No published items found");
+        } else {
+            resolve(publishedItems);
+        }
+    });
 }
 
 // Export the functions so that they can be used in server.js
 module.exports = {
+    initialize,
     getCategories,
-    getItems
+    getAllItems,
+    getPublishedItems
 };
